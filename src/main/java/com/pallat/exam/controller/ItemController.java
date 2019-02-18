8e44d41594da6009037762d2279116a18276be60
Pradeep <pradeep.gupta@ajatus.co.in>
2019-02-18 21:09:34 +0530

    Initial Commit

---
+++ src/main/java/com/pallat/exam/controller/ItemController.java
@@ -0,0 +1,51 @@
+package com.pallat.exam.controller;
+
+import com.pallat.exam.model.Item;
+import com.pallat.exam.repo.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+    @Autowired
+    ItemRepository itemRepo;
+
+    @RequestMapping("/getAllItems")
+    @ResponseBody
+    public List<Item> getAllItems() {
+        return itemRepo.getAllItems();
+    }
+
+    @RequestMapping("/updateItem")
+    @ResponseBody
+    public String updateItem(@RequestParam("itemId") int itemId, @RequestParam("name") String name) {
+        //return itemRepo.updateItem(itemId,name);
+        if (itemRepo.addItem(itemId,name) >= 1) {
+            return "Item Updated Successfully";
+        } else {
+            return "Something went wrong !";
+        } 
+    }
+
+    @RequestMapping("/addItem")
+    @ResponseBody
+    public String addItem(@RequestParam("id") int id, @RequestParam("name") String name) {
+        if (itemRepo.addItem(id, name) >= 1) {
+            return "Item Added Successfully";
+        } else {
+            return "Something went wrong !";
+        }
+    }
+
+    @RequestMapping("/deteteItem")
+    @ResponseBody
+    public String deteteItem(@RequestParam("itemId") int itemId) {
+        if (itemRepo.deleteItem(itemId) >= 1) {
+            return "Item Deleted Successfully";
+        } else {
+            return "Something went wrong !";
+        }
+    }
+}
---
+++ src/main/java/com/pallat/exam/repo/ItemRepository.java
@@ -0,0 +1,44 @@
+package com.pallat.exam.repo;
+
+import com.pallat.exam.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ItemRepository {
+
+    @Autowired
+    JdbcTemplate template;
+
+    /* Getting all Items from table */
+    public List<Item> getAllItems() {
+        List<Item> items = template.query("select Id, Name from Item", (result,
+                rowNum) -> new Item(result.getInt("id"), result.getString("name")));
+        return items;
+    }
+
+    /* Updating a specific item by item id from table */
+    public int updateItem(int itemId, String name) {
+    	System.out.println("inside update");
+        String query = "UPDATE Item SET Name = ? where id = ?";
+        //Item item = template.queryForObject(query, new Object[] { itemId }, new BeanPropertyRowMapper<>(Item.class));
+        return template.update(query, itemId, name );
+        //return item;
+    }
+
+    /* Adding an item into database table */
+    public int addItem(int id, String name) {
+        String query = "INSERT INTO Item VALUES(?,?)";
+        return template.update(query, id, name);
+    }
+
+    /* delete an item from database */
+    public int deleteItem(int id) {
+        String query = "DELETE FROM Item WHERE Id =?";
+        return template.update(query, id);
+    }
+}
